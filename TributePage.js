5e8e6be119a07f83dbfcebbbb582132be554be1e
hiralpatel85 <hirpatel2314@gmail.com>
2023-06-11 12:52:54 +0530

    sample commit

---
+++ TributePage.js
@@ -0,0 +1,11 @@
+const openNav = () => {
+    document.getElementById('sidenav').style.width = "350px";
+}
+
+const closeNav = () => {
+    document.getElementById('sidenav').style.width = "0px";
+}
+
+function redirectfunc(){
+    window.location.href ="https://en.wikipedia.org/wiki/A._P._J._Abdul_Kalam";
+}
